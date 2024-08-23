c8aea067b1aed62a0b2b439243235ae9c12c4d19
Raphael Sackey <>
2024-08-23 00:45:50 -0400

    first commit

---
+++ tests/app.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import App from '../src/App';
+
+describe("MyComponent", () => {
+  it('renders a button with the text "Click me"', () => {
+    render(<App />);
+    // Assert that the button with text "Click me" is in the document
+    expect(screen.getByText("Click me")).toBeInTheDocument();
+  });
+});
