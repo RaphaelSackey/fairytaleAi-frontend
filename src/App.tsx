1133c20caaf3e0674359c1d115385ca2e68bd10d
Raphael Sackey <>
2024-08-25 12:00:38 -0400

    commite to check ci pipline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import "./App.css";
+import logo from "./assets/logo.png";
 
 function App() {
- 
   return (
-      <button>Click me</button>
-
+    <div className="flex items-center justify-center h-screen bg-pageBackground">
+      <img src={logo} className="w-80 h-80"></img>
+      <div className="w-[70%] h-32 bg-componentBackground border border-componentBorder"></div>
+    </div>
   );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pageBackground: "#000",
+        componentBackground: "#0A0A0A",
+        componentBorder: "#2E2E2E",
+      },
+    },
   },
   plugins: [],
 };
